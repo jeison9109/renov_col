375e0f4dc66a8dddf632615dacf107096d47bd17
Yeison Serna <yeisonivanserna@gmail.com>
2021-09-30 12:08:10 -0500

    version 1.2.1

--- src/components/candidates/CandidatePerfil.js
+++ src/components/candidates/CandidatePerfil.js
@@ -28,35 +28,38 @@ export const CandidatePerfil = ({ history }) => {
 
   return (
     <>
+      <div className="fondcandi"></div>
       <div className="container-perfil">
         <div className="row">
-          <div className="col-4">
-            <img
-              src={`../assets/candidatos/${candidatoId}.jpg`}
-              className="card-img  animate__animated animate__fadeInLeft"
-              alt={candidato}
-            />
-          </div>
-          <div className="col-8">
-            <h3>{candidato}</h3>
-            <ul className="list-group list-group-flush">
-              <li className="list-group-item">
-                <b>Profesion: </b>
-                {alter_ego}
-              </li>
-              <li className="list-group-item">
-                <b>Departamento: </b>
-                {departamento}
-              </li>
-              <li className="list-group-item">{characters}</li>
-            </ul>
-            <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-              <button
-                className="btn btn-outline-info btn-sm"
-                onClick={handleReturn}
-              >
-                Regresar
-              </button>
+          <div className="card">
+            <div className="col-md-12">
+              <img
+                src={`../assets/candidatos/${candidatoId}.jpg`}
+                className="card-img  animate__animated animate__fadeInLeft"
+                alt={candidato}
+              />
+            </div>
+            <div className="col-8">
+              <h3>{candidato}</h3>
+              <ul className="list-group list-group-flush">
+                <li className="list-group-item">
+                  <b>Profesion: </b>
+                  {alter_ego}
+                </li>
+                <li className="list-group-item">
+                  <b>Departamento: </b>
+                  {departamento}
+                </li>
+                <li className="list-group-item">{characters}</li>
+              </ul>
+              <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                <button
+                  className="btn btn-outline-info btn-sm"
+                  onClick={handleReturn}
+                >
+                  Regresar
+                </button>
+              </div>
             </div>
           </div>
         </div>
--- src/components/screens/Contactenos.js
+++ src/components/screens/Contactenos.js
@@ -1,9 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import firebaseDb from "../../firebase";
+import { Formulario } from "./Formulario";
+import { Footer } from "../screens/Footer";
+import "../styles/contactenos.css";
 
 export const Contactenos = () => {
+  var [contactObjects, setContactObjects] = useState(0);
+  var [currentId, setCurrentId] = useState("");
+
+  useEffect(() => {
+    firebaseDb.child("contacts").on("value", (snapshot) => {
+      if (snapshot.val() != null)
+        setContactObjects({
+          ...snapshot.val(),
+        });
+    });
+  }, []);
+
+  const addOrEdit = (obj) => {
+    if (currentId == "")
+      firebaseDb.child("contacts").push(obj, (err) => {
+        if (err) console.log(err);
+        else setCurrentId("");
+      });
+    else
+      firebaseDb.child(`contacts/${currentId}`).set(obj, (err) => {
+        if (err) console.log(err);
+        else setCurrentId("");
+      });
+  };
+
   return (
-    <div>
-      <h1>Contactenos</h1>
-    </div>
+    <>
+      <div className="container-form">
+        <h1>Contactenos</h1>
+      </div>
+
+      <Formulario {...{ addOrEdit, currentId, contactObjects }} />
+
+      <Footer />
+    </>
   );
 };
